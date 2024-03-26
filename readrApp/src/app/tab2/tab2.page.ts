6e9ac571b23adb547213203b857cd6b3bc4c3d32
soulcell <arsen332211@gmail.com>
2024-03-26 13:25:09 +0200

    implemented reload function

--- readrApp/src/app/tab2/tab2.page.ts
+++ readrApp/src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, QueryList, ViewChildren } from '@angular/core';
+import { Component, ElementRef, HostBinding, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { HammerModule } from '@angular/platform-browser';
@@ -21,7 +21,7 @@ import { IonButton, IonContent, IonHeader, IonIcon, IonToolbar } from '@ionic/an
     IonHeader, IonToolbar, IonContent
   ],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
 
   @HostBinding('style.--card-translation-x')
@@ -46,29 +46,7 @@ export class Tab2Page {
   bookCards!: QueryList<ElementRef>;
 
 
-  cards: Array<BookModel> = [
-    {
-      id: 0,
-      title: "The Picture of Dorian Gray",
-      authorName: "Oscar Wilde",
-      imageUrl: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781625587534/the-picture-of-dorian-gray-9781625587534_hr.jpg",
-      distanceMeters: 1000
-    },
-    {
-      id: 2,
-      title: "The Great Gatsby",
-      authorName: "F. Scott Fitzgerald",
-      imageUrl: "https://i0.wp.com/americanwritersmuseum.org/wp-content/uploads/2018/02/CK-3.jpg?resize=267%2C400&ssl=1",
-      distanceMeters: 500
-    },
-    {
-      id: 3,
-      title: "The Glass Bead Game",
-      authorName: "Hermann Hesse",
-      imageUrl: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1386922806i/16634.jpg",
-      distanceMeters: 1400
-    },
-  ];
+  cards: Array<BookModel> = [];
 
   handlePan(event: any) {
     if (event.deltaX === 0 || (event.center.x === 0 && event.center.y === 0)) return;
@@ -109,6 +87,36 @@ export class Tab2Page {
 
   constructor() {
     addIcons({refreshCircleOutline})
+  }
+
+  ngOnInit() {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.cards = [
+      {
+        id: 0,
+        title: "The Picture of Dorian Gray",
+        authorName: "Oscar Wilde",
+        imageUrl: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781625587534/the-picture-of-dorian-gray-9781625587534_hr.jpg",
+        distanceMeters: 1000
+      },
+      {
+        id: 2,
+        title: "The Great Gatsby",
+        authorName: "F. Scott Fitzgerald",
+        imageUrl: "https://i0.wp.com/americanwritersmuseum.org/wp-content/uploads/2018/02/CK-3.jpg?resize=267%2C400&ssl=1",
+        distanceMeters: 500
+      },
+      {
+        id: 3,
+        title: "The Glass Bead Game",
+        authorName: "Hermann Hesse",
+        imageUrl: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1386922806i/16634.jpg",
+        distanceMeters: 1400
+      },
+    ]
   }
 
 }
